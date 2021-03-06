88444606a81bdaeeb6b74e39723da5907c6ba469
Aman Srivastava <aman4863@gmail.com>
2021-03-06 20:05:20 +0530

    functions

---
+++ lec1_js_intro/functions.js
@@ -0,0 +1,21 @@
+function sayHi(){
+    console.log(" Aman says Hii!!");
+}
+
+console.log(sayHi());   //Aman says Hii!!
+console.log(sayHi);     // It will tell the nature of sayHi     [Function: sayHi]
+
+//functions are variables
+
+// let fnc = function{
+//     console.log(" Aman says Hii!!");
+// }
+
+//=========================================================
+//function with parameters
+
+function intro(name){
+    console.log(name + " says Hii!!");
+}
+
+intro("Aman");
---
+++ lec1_js_intro/hof.js
@@ -0,0 +1,20 @@
+// High Order Functions => Functions which accept functions as a argument !
+// Callback Functions => Functions which are passed into a function as a argument !
+
+function getFirstName(fullName){
+    // fullName = "TONY STARK".split(" ") => [ "TONY" , "STARK"  ];
+    fullName = fullName.split(" ");
+    return fullName[0];
+}
+function getLastName(fullName){
+    fullName = fullName.split(" ");
+    return fullName[1];
+}
+
+function sayHi(fullName, fun){
+    let name = fun(fullName);
+    console.log(name);
+}
+
+sayHi("Tony Stark", getFirstName)
+sayHi("Aman Srivastava", getLastName)
