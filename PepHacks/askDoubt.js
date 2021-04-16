a3e640737881595236cc7db208e4c035afd15db3
Aman Srivastava <aman4863@gmail.com>
2021-04-16 18:11:48 +0530

    todo started

--- PepHacks/askDoubt.js
+++ PepHacks/askDoubt.js
@@ -1,6 +1,6 @@
 const puppeteer = require("puppeteer");
-const id = "*************";   //Your Email Here
-const pw = "*************";   //Your Password Here
+const id = "aman4863@gmail.com";   //Your Email Here
+const pw = "dingdong";   //Your Password Here
 const name = "Aman Srivastava";   //Your name here
 const batchName = "DevPP";
 const msg = "Hi! I have doubt in DSA! \n My name is "+ name + "\n Batch: "+ batchName +"\n Please send me a zoom link!";
@@ -27,19 +27,35 @@ let browser;
   await tab.waitForSelector('a[id="showLiveDoubtSlideOut"]' , {visible:true});
   await tab.click('a[id="showLiveDoubtSlideOut"]');
   
-  await tab.waitForTimeout(5000); 
-  await tab.waitForSelector('a[data-target="slide-out-admin-chat"]' , {visible:true});
-  await (await tab.$('a[data-target="slide-out-admin-chat"]')).click();
-  //navigated to tawk.to
+  // await tab.waitForTimeout(5000); 
+  // await tab.waitForSelector('.additionalPadding #chatTextarea' , {visible:true});
+  // // await (await tab.$('a[data-target="slide-out-admin-chat"]')).click();
+  // //navigated to tawk.to
+  
+  
+  // // await page.$eval('#email', el => el.value = 'test@example.com');
+
+  // await tab.waitForSelector('.additionalPadding textarea' , {visible:true});
+  // await tab.type('.additionalPadding textarea', msg);
+
+
+
+  
+
+
+  // await tab.waitForTimeout(5000); 
+  // await tab.waitForSelector('fieldset .additionalPadding' , {visible:true});
+  // await (await tab.$('fieldset .additionalPadding')).click();
+  // fieldset .additionalPadding
 
   await tab.waitForTimeout(3000); 
-  await tab.waitForSelector('#chatTextarea' , {visible:true});
-  await tab.type('#chatTextarea', msg);
+  await tab.waitForSelector('textarea#chatTextarea' , {visible:true});
+  await tab.type('textarea#chatTextarea', msg);
   await tab.keyboard.press("Enter");
   //requested successfully for a zoom link!
   }
   catch(error){
     await console.log(error);
-    await browser.close();
+    // await browser.close();
   }
 })();
