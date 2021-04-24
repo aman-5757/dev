0b4a6581d53ef559f22f774286b8f4b261b7379c
Aman Srivastava <aman4863@gmail.com>
2021-04-24 20:28:38 +0530

    excel part 1

--- Module_02_DOM/Lec_02_TicketManagement/activity/script.js
+++ Module_02_DOM/Lec_02_TicketManagement/activity/script.js
@@ -15,6 +15,7 @@ let filterCodes = {
   
   function loadTickets(){
     if(localStorage.getItem("allTickets")){
+        ticketContainer.innerHTML = "";
       let allTickets = JSON.parse(localStorage.getItem("allTickets"));
       for(let i=0 ; i<allTickets.length ; i++){
         let {ticketId , ticketFilter , ticketContent} = allTickets[i];
@@ -95,7 +96,7 @@ let filterCodes = {
     // set selected filter as the now choose filter !!!
     selectedFilter = selectedModalFilter;
     // remove active filter class
-    document.querySelector(".active-filter").classList.remove("active-filter");
+    document.querySelector(".modal-filter.active-filter").classList.remove("active-filter");
     // add active filter class on now selected filter 
     e.target.classList.add("active-filter");
   }
@@ -161,7 +162,49 @@ let filterCodes = {
     allFilters[i].addEventListener("click", chooseFilter);
   }
   function chooseFilter(e) {
-    let filter = e.target.classList[1];
-    let filterCode = filterCodes[filter];
-    ticketContainer.style.background = filterCode;
+    if(e.target.classList.contains("active-filter")){
+        //if active filter already present!
+        e.target.classList.remove("active-filter");
+        loadTickets();
+        return;
+    }
+
+    //remove active filter from already selected filter
+    if(document.querySelector(".filter.active-filter")){
+        document.querySelector(".filter.active-filter").classList.remove("active-filter");
+    }
+
+    //add active filter on now selected filter!!
+    e.target.classList.add("active-filter");
+    let ticketFilter = e.target.classList[1];
+    loadSelectedTickets(ticketFilter);
   }
+
+  function loadSelectedTickets(ticketFilter){
+    if(localStorage.getItem("allTickets")){
+      let allTickets = JSON.parse(localStorage.getItem("allTickets"));
+  
+      let filteredTickets = allTickets.filter( function(filterObject){
+        return filterObject.ticketFilter == ticketFilter;
+      });
+  
+      // console.log(filteredTickets);
+  
+      // empty tickets container
+      ticketContainer.innerHTML = "";
+      for(let i=0 ; i<filteredTickets.length ; i++){
+        let {ticketId , ticketFilter , ticketContent} = filteredTickets[i];
+  
+        let ticketDiv = document.createElement("div");
+        ticketDiv.classList.add("ticket");
+        // set the html of the ticket wala div !!
+        ticketDiv.innerHTML = ` <div class="ticket-filter ${ticketFilter}"></div>
+        <div class="ticket-id">#${ticketId}</div>
+        <div class="ticket-content">${ticketContent}</div>`;
+  
+        // append the ticket on the UI !!!!
+        ticketContainer.append(ticketDiv);
+      }
+  
+    }
+  }  
